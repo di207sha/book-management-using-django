152bfdf3037f1ee9c325304d7d8c88a48c6db8ab
di207sha <dishalunagariya09@gmail.com>
2024-08-31 14:22:50 +0530

    creating model named BOOk and creating superuser named admin

--- library/models.py
+++ library/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 class Book(models.Model):
-    title=models.CharField(max_length=50)
-    price=models.IntegerField()
+    isbn_no=models.IntegerField()
+    book_name=models.CharField(max_length=50)
+    author_name=models.CharField(max_length=50)
+    published_year=models.IntegerField()
+
--- library/views.py
+++ library/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 # Create your views here.
 def hello(request):
     return render(request,"firstpage.html")
@@ -10,4 +11,16 @@ def viewbook(request):
 def login(request):
     return render(request,"login.html")
 def addbook(request):
+    if request.method=="POST":
+        i=request.POST["isbn no"]
+        t=request.POST["title"]
+        a=request.POST["author"]
+        y=request.POST["year"]
+        book=Book()
+        book.isbn_no=i
+        book.book_name=t
+        book.author_name=a
+        book.published_year=y
+        book.save()
+        return HttpResponse("/add-book")
     return render(request,"addbook.html")
